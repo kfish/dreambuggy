aed37acb7ff5b83fd5940d898ffa8cc05dc1fe03
Conrad Parker <conrad@metadecks.org>
2017-07-31 07:23:36 +0800

    demo: Remove engine code

--- engine/Here4/Navigator/Control.elm
+++
@@ -1,7 +0,0 @@
-module Here4.Navigator.Control exposing (..)
-
-import Here4.Dispatch exposing (..)
-import Here4.Model exposing (NavigatorMsg)
-
-type alias NavMsg a =
-    Dispatch NavigatorMsg () a
--- engine/Here4/Vehicle.elm
+++
@@ -1,9 +0,0 @@
-module Here4.Vehicle exposing (..)
-
-
-type alias Driveable a =
-    { a
-        | speed : Float
-        , height : Float
-        , radius : Float
-    }
